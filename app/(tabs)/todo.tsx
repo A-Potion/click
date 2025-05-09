8010da980343b2e437cd83d1ea6f89522538d02e
Kacper <07Potion@gmail.com>
2025-05-08 22:06:57 -0400

    Working on swipe list

--- app/(tabs)/todo.tsx
+++ app/(tabs)/todo.tsx
@@ -23,6 +23,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         gap: '1%',
+        flex: 1,
     },
     scroll: {
         flex: 1,
--- components/ToDoTile.tsx
+++ components/ToDoTile.tsx
@@ -2,11 +2,18 @@ import { Text, View, StyleSheet } from 'react-native';
 import Checkbox from 'expo-checkbox';
 import { useState } from 'react';
 
+import Swiper from 'react-native-swiper';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { JumpingTransition } from 'react-native-reanimated';
+
 export default function ToDoTile() {
     const [isChecked, setIsChecked ] = useState(false);
 
     return(
-        <View style={styles.container}>
+        <View style={styles.centralContainer} >
+            <Swiper autoplay={false} loop={false} style={styles.swiper} showsButtons={false}>
+            <View style={styles.container}>
             <View style={styles.titleContainer}>
                 <Text style={styles.title}>
                     Lorem ipsum dolor sit amet.
@@ -18,8 +25,20 @@ export default function ToDoTile() {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </Text>
                 </View>
-                <Checkbox value={isChecked} onValueChange={setIsChecked} style={styles.checkbox} />
+            <View style={styles.arrowContainer}>
+                <Ionicons name='arrow-back' size={34} />
+                <Text>
+                    Mark done
+                </Text>
             </View>
+            </View>
+            </View>
+            <View style={styles.doneStyle}>
+                <Text>
+                    Meow
+                </Text>
+            </View>
+            </Swiper>
         </View>
     )
 }
@@ -33,6 +52,8 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         paddingBottom: 6,
         paddingTop: 6,
+        height: 'auto',
+
     },
     titleContainer: {
 
@@ -51,10 +72,29 @@ const styles = StyleSheet.create({
         alignContent: 'space-between',
         width: '94%',
     },
-    checkbox: {
-        transform: 'scale(1.5)',
-    },
     description: {
         fontSize: 20,
+    },
+    arrowContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    swiper: {
+        alignContent: 'center',
+        alignSelf: 'center',
+    }, 
+    doneStyle: {
+        backgroundColor: '#21d1a5',
+        width: '96%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 0,
+        paddingLeft: 0,
+        paddingBottom: 6,
+        paddingTop: 6,
+    },
+    centralContainer: {
+
     }
 })
