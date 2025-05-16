6b1366bd5043686d44a0ede3dd04ba4fa1f49ed4
Kacper <07Potion@gmail.com>
2025-05-15 20:59:19 -0400

    Delete on swipe kinda works.

--- components/ToDoTile.tsx
+++ components/ToDoTile.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Animated } from 'react-native';
 import Checkbox from 'expo-checkbox';
 import { useState } from 'react';
 
@@ -9,50 +9,59 @@ import { JumpingTransition } from 'react-native-reanimated';
 
 
 export default function ToDoTile() {
-    const listViewData = Array(20)
-    .fill("")
-    .map((_, i) => ({ key: `${i}`, text: `item #${i}` }));
+const [listData, setListData] = useState(
+        Array(20)
+            .fill('')
+            .map((_, i) => ({ key: `${i}`, text: `item #${i}` }))
+    );
+
     
     return(
         <SwipeListView
-        style={styles.slv}
-        data={listViewData}
-        renderItem={ (data, rowMap) =>
-            <View style={styles.container}>
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>
-                    Lorem ipsum dolor sit amet.
-                </Text>
-            </View>
-            <View style={styles.descriptionContainer}>
-                <View style={styles.textContainer}>
-                    <Text style={styles.description}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </Text>
+            style={styles.slv}
+            data={listData}
+            keyExtractor={data => data.key}
+            renderItem={(data) =>
+                <View style={styles.container}>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>
+                            Lorem ipsum dolor sit amet. {data.item.key}
+                        </Text>
+                    </View>
+                    <View style={styles.descriptionContainer}>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.description}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </Text>
+                        </View>
+                        <View style={styles.arrowContainer}>
+                            <Ionicons name='arrow-back' size={34} />
+                            <Text>
+                                Mark done
+                            </Text>
+                        </View>
+                    </View>
                 </View>
-            <View style={styles.arrowContainer}>
-                <Ionicons name='arrow-back' size={34} />
-                <Text>
-                    Mark done
-                </Text>
-            </View>
-            </View>
-            </View>
-        }
-        renderHiddenItem={ ( data, rowMap ) =>
-            <View style={styles.doneStyle}>
-            <Ionicons name='checkmark-outline' size={70} />
-            </View>
-            
-        }
-        rightOpenValue={-75}
-        disableRightSwipe={true}
-        stopRightSwipe={-110}
-        ItemSeparatorComponent={() => <View style={{height: 6}} />}
-        showsVerticalScrollIndicator={false}
->
-           
-            </SwipeListView>
+            }
+            renderHiddenItem={(data, rowMap) =>
+                <View style={styles.doneStyle}>
+                    <Ionicons name='checkmark-outline' size={70} />
+                </View>
+            }
+            disableRightSwipe={true}
+            stopRightSwipe={-110}
+            ItemSeparatorComponent={() => <View style={{ height: 6 }} />}
+            showsVerticalScrollIndicator={false}
+            rightActivationValue={-30}
+            rightOpenValue={-20}
+            onSwipeValueChange={(swipeData) => {
+                const { key, value } = swipeData;
+                if (value < -70) {
+                    const newData = listData.filter(item => item.key !== key);
+                    setListData(newData);
+                }
+            }}
+        />
     )
 }
 
